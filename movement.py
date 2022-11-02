2df40ba96116f556eb9f3395a908bcfd3683cd11
Bryce McNealey <bmcnealey3@gatech.edu>
2022-11-02 16:17:50 -0400

    added new funtionality to integration.py

--- movement.py
+++ movement.py
@@ -1,5 +1,8 @@
+from gpiozero import AngularServo
 # This code simulates pressing the movement UI botton on the box.
 def start():
+    servo = AngularServo(17, min_angle=-720, max_angle=720)
+    servo.angle = -90
     print("teapot opened")
     print("mouse moved up")
     print("mouse lowered")
