92e8e81da7917eb8dd8dec1f98e430eeeabac25b
Bryce McNealey <bmcnealey3@gatech.edu>
2022-11-02 17:07:32 -0400

    new branch

--- integration.py
+++ integration.py
@@ -1,15 +1,42 @@
+from signal import pause
+from gpiozero import Button, AngularServo
 import movement
 import lights
 import audio
 import connection
 import integration
-from gpiozero import Button, AngularServo
-from signal import pause
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
 
 def start(test):
-    servo = AngularServo(17, min_angle=-720, max_angle=720)
-    servo.angle = -90
+
+
+try:
+    while True:
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
     print("teapot opens up")
     print("mouse moved")
     print("lights flash")
